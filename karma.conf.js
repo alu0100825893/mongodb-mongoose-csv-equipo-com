3e65d44050d89e46254ad68efda1bd729d7e76af
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2016-04-07 22:48:20 +0100

    arreglando travis

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'cvs.js',      
+      'csv.js',      
       'tests/*.js'
     ],
     // list of files to exclude
