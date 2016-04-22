27031f7b0ddcddb8c4310034c6e5216c2a77cccc
alu0100886870 <alu0100886870@ull.edu.es>
2016-04-22 03:15:31 +0000

    Terminando de agrupar las funciones relacionadas con la database

--- database.js
+++ database.js
@@ -1,9 +1,14 @@
+"use strict";
+
 var mongoose = require('mongoose');
 module.exports.mongoose = mongoose;
 
 //Declarar variables
 var Csv;
 var CsvSchema;
+
+
+//Funcion para iniciar el esquema y modelo a utilizar en la DB
 
 var iniciar = module.exports.iniciar = function() {
     
@@ -33,6 +38,83 @@ var getBotones = module.exports.getBotones = function(res) {
     Csv.find({}, (err, finded) => {
         findedEnv = finded;
         res.render('layout', { title: 'CSV ajax', botones: findedEnv})
+        mongoose.connection.close();
+    });
+    
+}
+
+//Función para guardar una entrada en la base de datos
+var guardarEntrada = module.exports.guardarEntrada = function(req) {
+    mongoose.connect('mongodb://localhost/baseDatos');
+    
+    let numEntradas;
+    let numReg;
+    let elementoAct;
+    
+    Csv.find({}, (err, finded) => {
+        console.log("error",err)
+        numEntradas = finded.length;
+        console.log("num entradas",numEntradas);
+    }).then( (value) => {
+        if(numEntradas < 4) {
+            //Se pueden crear registros
+            numReg = numEntradas + 1;
+            if (numEntradas == 0) {
+                elementoAct = true;
+            }
+            else {
+                elementoAct = false;
+            }
+            
+            let csvTemp = new Csv({
+                "nombre": req.query.nombre,
+                "contenido": req.query.contenido,
+                "numeroRegistro" : numReg,
+                "elementoActual" : elementoAct
+            });
+            
+            let p1 = csvTemp.save( (err) => { 
+              if (err) console.log("Algo ha ido mal en el guardado");
+            });
+            
+            p1.then( (value) => {
+                console.log("Guardado: " + value.nombre);
+                mongoose.connection.close();
+            });
+            
+        }
+        else {
+            //Se reutilizan registros
+            console.log("reutilizando reg");
+            
+            Csv.find({elementoActual : true}, (err, finded) => {
+                numReg = finded[0].numeroRegistro;
+            }).then( (value) => {
+                let siguiente = (numReg + 1) % 4;
+                if (siguiente == 0) siguiente = 4;
+                //Actualizando registro actual y el siguiente
+                Csv.update({numeroRegistro : siguiente},{elementoActual : true}, () => {
+                    Csv.update({numeroRegistro : numReg},
+                        {nombre: req.query.nombre, contenido : req.query.contenido, elementoActual : false},
+                        () => {
+                            mongoose.connection.close();
+                        })
+                })
+            })
+            
+        }
+    })
+}
+
+//Lee la info del boton
+var queryBoton = module.exports.queryBoton = function(req, res) {
+    
+     mongoose.connect('mongodb://localhost/baseDatos');
+    console.log(req.query.botonId)
+    let request = req.query.botonId;
+    Csv.find({numeroRegistro: request}, (err, finded) => { 
+        let answer = finded[0].contenido;
+        res.send(answer);
         mongoose.connection.close();
     });
     
--- hello.js
+++ hello.js
@@ -6,10 +6,7 @@ var app = express()
 // https://nodejs.org/api/path.html
 var path = require("path")
 var mongoose = require('mongoose');
-
-
-
-
+var database = require('./database.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -42,32 +39,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // is the route that express uses when we visit
 // our site initially.
 
-//Conectando a una base de datos 
-mongoose.connect('mongodb://localhost/baseDatos');
-
-//Elementos de la base de datos
-var CsvSchema = mongoose.Schema({
-        "nombre" : String,
-        "contenido" : String,
-        "numeroRegistro" : Number,
-        "elementoActual" : Boolean
-});
-
-
-var Csv = mongoose.model("Csv", CsvSchema);
-
-mongoose.connection.close();
+database.iniciar();
 
 app.get('/', (req, res)  => {
     
-    mongoose.connect('mongodb://localhost/baseDatos');
+    database.getBotones(res);
     
-    var findedEnv
-    Csv.find({}, (err, finded) => {
-        findedEnv = finded;
-        res.render('layout', { title: 'CSV ajax', botones: findedEnv})
-        mongoose.connection.close();
-    });
 });
 
 
@@ -78,77 +55,13 @@ app.get('/calculate', (req, res) =>{
 });
 
 app.get('/mongo/queryBoton', (req, res) =>{
-    mongoose.connect('mongodb://localhost/baseDatos');
-    console.log(req.query.botonId)
-    let request = req.query.botonId;
-    Csv.find({numeroRegistro: request}, (err, finded) => { 
-        let answer = finded[0].contenido;
-        res.send(answer);
-        mongoose.connection.close();
-    });
+   database.queryBoton(req, res);
 
 });
 
 app.get('/mongo/save', (req, res) => {
-    mongoose.connect('mongodb://localhost/baseDatos');
     
-    let numEntradas;
-    let numReg;
-    let elementoAct;
-    
-    Csv.find({}, (err, finded) => {
-        console.log("error",err)
-        numEntradas = finded.length;
-        console.log("num entradas",numEntradas);
-    }).then( (value) => {
-        if(numEntradas < 4) {
-            //Se pueden crear registros
-            numReg = numEntradas + 1;
-            if (numEntradas == 0) {
-                elementoAct = true;
-            }
-            else {
-                elementoAct = false;
-            }
-            
-            let csvTemp = new Csv({
-                "nombre": req.query.nombre,
-                "contenido": req.query.contenido,
-                "numeroRegistro" : numReg,
-                "elementoActual" : elementoAct
-            });
-            
-            let p1 = csvTemp.save( (err) => { 
-              if (err) console.log("Algo ha ido mal en el guardado");
-            });
-            
-            p1.then( (value) => {
-                console.log("Guardado: " + value.nombre);
-                mongoose.connection.close();
-            });
-            
-        }
-        else {
-            //Se reutilizan registros
-            console.log("reutilizando reg");
-            
-            Csv.find({elementoActual : true}, (err, finded) => {
-                numReg = finded[0].numeroRegistro;
-            }).then( (value) => {
-                let siguiente = (numReg + 1) % 4;
-                if (siguiente == 0) siguiente = 4;
-                //Actualizando registro actual y el siguiente
-                Csv.update({numeroRegistro : siguiente},{elementoActual : true}, () => {
-                    Csv.update({numeroRegistro : numReg},
-                        {nombre: req.query.nombre, contenido : req.query.contenido, elementoActual : false},
-                        () => {
-                            mongoose.connection.close();
-                        })
-                })
-            })
-            
-        }
-    })
+    database.guardarEntrada(req);
     
 });
 
